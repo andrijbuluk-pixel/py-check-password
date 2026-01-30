58ce7a21a9f117290ca03c4864a3f9d44579554e
Andriy <andrijbuluk@gmail.com>
2026-01-30 10:52:04 +0100

    Check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,39 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        pytest.param(
+            "Numb$r3",
+            False,
+            id="Minimum length check"
+        ),
+        pytest.param(
+            "Numb$r33",
+            False,
+            id="Digit check"
+        ),
+        pytest.param(
+            "Number33",
+            False,
+            id="Special character check"
+        ),
+        pytest.param(
+            "numb$r33",
+            False,
+            id="Check for capitalization"
+        ),
+        pytest.param(
+            "Pass@word1",  # правильний пароль
+            True,
+            id="Valid password"
+        )
+    ]
+)
+def test_check_password(
+        password: str,
+        expected: bool
+) -> None:
+    assert check_password(password) == expected
